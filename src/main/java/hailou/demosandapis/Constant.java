fbe9514a1d1d4ab04aca3a21ad3506dbb632b889
hailuwang <hailushijie@gmail.com>
2017-07-24 20:35:15 +0800

    Commit Base Android Studio Moudle
    Signed-off-by: hailuwang <hailushijie@gmail.com>

---
+++ src/main/java/hailou/demosandapis/Constant.java
@@ -0,0 +1,7 @@
+package hailou.demosandapis;
+
+public class Constant {
+    public static final String DemosForApiPath = "heilouwang.demosforapi.path";
+    public static final String CategoryForDemosApi = "hailouwang.demosforapi.intent.category.DEMOS_API";
+    public static final boolean isFinishActivityTask = false;
+}
